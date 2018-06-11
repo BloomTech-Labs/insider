6cae9dab202acad67e687bb5e9bc23fce07f93ce
Peter Gray <peter@petergraycreative.com>
2018-06-11 15:57:18 -0500

    Finish setting up airbnb linting
    Needed to uninstall globally and install locally

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
+
 const server = express();
 const router = express.Router();
 
 server.use(express.json());
 
 /* GET home page. */
-server.get('/', function(req, res) {
-    res.send('Anonymous Texts');
+server.get('/', (req, res) => {
+  res.send('Anonymous Texts');
 });
 
-server.listen(3000, () => console.log('Example app listening on port 3000!'))
+server.listen(3000, () => console.log('Example app listening on port 3000!'));
