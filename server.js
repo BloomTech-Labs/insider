7feace7e1b651f94058f9c41a575d51a7a1ee18b
Peter Gray <peter@petergraycreative.com>
2018-06-11 12:46:31 -0500

    Basic express server setup
    Add basic endpoint, express, fetch packages

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+const router = express.Router();
+
+server.use(express.json());
+
+/* GET home page. */
+server.get('/', function(req, res) {
+    res.send('Anonymous Texts');
+});
+
+server.listen(3000, () => console.log('Example app listening on port 3000!'))
