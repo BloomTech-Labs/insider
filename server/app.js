0392daf3de312cad781702d270873addf82cae32
Richard Reis <richardreeze1@gmail.com>
2018-06-13 10:54:21 -0700

    fixing express issue

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const app = require('./server');
+//const app = require('./server');
+const app = express();
 
 // Imports server.js and app.js creates a connection containing the routes and middleware
 
