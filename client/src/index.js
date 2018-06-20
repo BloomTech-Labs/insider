d6151506b874a4895563bca1879aeb4e501eb43a
Igor Yermak <igor.yermak@gmail.com>
2018-06-20 13:41:01 -0400

    Added some Bootstrap styling
    - styled button
    - added `To:` and `Message` as headers
    - styled the input forms with bootstrap
    - added bootstrap library 
    - centered layout and added border for components
    - added our logo color and a background white as variables in our vars.less file

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 /*eslint-disable */
 import './css/bootstrap-reboot.min.css';
 import './css/bootstrap-grid.min.css';
+import './css/bootstrap.min.css'; 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.css';
