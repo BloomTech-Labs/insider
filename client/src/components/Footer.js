8eaf74ef5bdb1ac8872dd1a22b4bc0e65c211625
Richard Reis <richardreeze1@gmail.com>
2018-06-28 14:12:00 -0700

    built gorgeous footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = (props) => {
+  return (
+    <div className="footer">
+      <img src="images/lambda_logo.png" alt="Lambda logo" />
+      <p>●</p>
+      <p>Contact us</p>
+    </div>
+  )
+}
+
+export default Footer;
