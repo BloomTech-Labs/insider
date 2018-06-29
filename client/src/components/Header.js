4544930819d484971a2a3fef7d32b541e7a67a44
Peter Gray <peter@petergraycreative.com>
2018-06-28 22:27:52 -0500

    Fixed header responsiveness
    Text size now grows and logo/text is centered on mobile

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,20 +3,22 @@ import React from 'react';
 const Footer = () => {
   return (
     <nav className="navbar navbar-expand-sm sticky-top">
-      <a className="navbar-brand" href="/">
-        <img
-          id="white-logo"
-          src="/images/ghost-texts-heavy.svg"
-          alt="White Logo"
-        />
-      </a>
-      <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-        <li className="nav-item">
-          <a className="nav-link" href="/contact">
-            Contact
-          </a>
-        </li>
-      </ul>
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img
+            id="white-logo"
+            src="/images/ghost-texts-heavy.svg"
+            alt="White Logo"
+          />
+        </a>
+        <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" href="/contact">
+              Contact The Team
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
