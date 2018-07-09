98a2de762d4817c771e601ffa1fba07c9191b1af
Peter Gray <peter@petergraycreative.com>
2018-07-09 18:01:52 -0500

    Attempting to debug Heroku server

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')), apiRoutes);
 
 // You can add in any routes you want as you import them
 app.use('/api', envCheck, apiRoutes);
