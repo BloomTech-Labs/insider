cc9b76be3321123e9b93b70b224023d12e6c350a
Peter Gray <peter@petergraycreative.com>
2018-07-13 13:06:54 -0500

    Update API URLs to our new domain
    Now point to www.ghosttexts.com

--- client/src/components/MessageFeed.js
+++ client/src/components/MessageFeed.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client';
 const apiURI =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3030/'
-    : 'https://anonymous-texts.herokuapp.com/';
+    : 'http://www.ghosttexts.com/';
 const socket = io(apiURI);
 
 type State = {
--- server/models/models.js
+++ server/models/models.js
@@ -30,7 +30,7 @@ const sendSMS = (message, recipient) => {
       body: message,
       to: recipient,
       from: TWILIO_FROM,
-      statusCallback: 'https://anonymous-texts.herokuapp.com/api/twilio-status/',
+      statusCallback: 'http://www.ghosttexts.com/api/twilio-status/',
     })
     .then(response => response)
     .catch(err => err);
