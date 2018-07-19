8a395b21b3d7a46f917dec2e333e19e7a1b22326
Peter Gray <peter@petergraycreative.com>
2018-07-19 10:16:09 -0500

    Testing dev URLS

--- client/src/components/MessageFeed.js
+++ client/src/components/MessageFeed.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Message from './Message';
 import io from 'socket.io-client';
 
-const apiURI = '//www.ghosttexts.com/';
+const apiURI = 'https://anonymous-texts.herokuapp.com/';
 const socket = io(apiURI);
 
 type State = {
